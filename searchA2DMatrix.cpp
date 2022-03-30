aeeeae217c773d9654054f4c414d4ed7eab134b2
aayushkabra1 <ayushkabra1212@gmail.com>
2022-03-30 11:05:05 +0530

    added second approach to leetcode.com/problems/search-a-2d-matrix

--- searchA2DMatrix.cpp
+++ searchA2DMatrix.cpp
@@ -20,3 +20,32 @@ using namespace std;
 //     }
 // };
 
+// Approach 2
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n =  matrix.size();
+        int m = matrix[0].size();
+        int r = 0;
+        int c = m - 1;
+
+        while(r < n && r >= 0 && c >= 0 && c < m) {
+            int temp = matrix[r][c];
+            if (temp > target) c--;
+            if (temp < target) r++;
+            if (temp == target) return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    vector<vector<int>> matrix = {{1}};
+    int target = 0;
+    cout << sol.searchMatrix(matrix, target) << endl;
+    return 0;
+}
+
+
