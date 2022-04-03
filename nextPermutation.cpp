b2d418ee7922e1011af5f2dcdc1f0c3cd960b37c
aayushkabra1 <ayushkabra1212@gmail.com>
2022-04-03 09:15:21 +0530

    added approach 2 for leetcode.com/problems/next-permutation

--- nextPermutation.cpp
+++ nextPermutation.cpp
@@ -3,9 +3,40 @@
 #include <algorithm>
 using namespace std;
 
+// Approach 1
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
+//         next_permutation(nums.begin(), nums.end());
+//     }
+// };
+
+//Approach 2
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+
+        int peak = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i + 1] > nums[i]) {
+                peak = i;
+                break;
+            }
+        }
+
+        if (peak == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        int i;
+        for (i = n - 1; i > peak; i--) {
+            if (nums[i] > nums[peak]) {
+                break;
+            }
+        }
+        swap(nums[peak], nums[i]);
+        reverse(nums.begin() + peak + 1, nums.end());
     }
 };
