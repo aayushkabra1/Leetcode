d2db655b2039e13cda97ebe084149ff24c1eecf7
aayushkabra1 <ayushkabra1212@gmail.com>
2022-04-04 11:48:05 +0530

    leetcode.com/problems/best-time-to-buy-and-sell-stock

--- bestTimeToBuyAndSellStock.cpp
+++ bestTimeToBuyAndSellStock.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxDiff = prices[1] - prices[0], minPrice = prices[0], n = prices.size();
+        int maxDiff = 0, minPrice = prices[0], n = prices.size();
         for (int i = 1; i < n; i++) {
             maxDiff = max(maxDiff, prices[i] - minPrice);
             minPrice = min(prices[i], minPrice);
