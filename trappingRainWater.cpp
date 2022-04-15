fb1c961731a9b806ca6013d593c1d211efeb9b0d
aayushkabra1 <ayushkabra1212@gmail.com>
2022-04-15 18:08:58 +0530

    leetcode.com/problems/trapping-rain-water

--- trappingRainWater.cpp
+++ trappingRainWater.cpp
@@ -1,18 +1,39 @@
-#include "bits/stdc++.h"
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
     int trap(vector<int>& height) {
-        stack<int> st;
-        while()
+        int n = height.size();
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+        left[0] = height[0], right[n - 1] = height[n - 1];
+        for (int i = 1; i < n; i++) {
+            left[i] = max(left[i - 1], height[i]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = max(right[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (left[i] > height[i] && right[i] > height[i]) {
+                ans += min(left[i], right[i]) - height[i];
+            }
+        }
+        return ans;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    
+    Solution sol;
+    vector<int> height = {4,2,0,3,2,5};
+    cout << sol.trap(height);
     return 0;
 }
+
+
 
 
