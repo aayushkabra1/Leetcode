c81f04b9e0ad6db3ed2d9f91e41ddad7cf3e6b66
aayushkabra1 <ayushkabra1212@gmail.com>
2022-05-02 08:21:29 +0530

    added second approach leetcode.com/problems/sort-array-by-parity

--- sortArrayByParity.cpp
+++ sortArrayByParity.cpp
@@ -3,14 +3,26 @@
 #include <algorithm>
 using namespace std;
 
+// class Solution {
+// public:
+//     static bool cmp(int a, int b) {
+//         return (a % 2) < (b % 2);
+//     }
+
+//     vector<int> sortArrayByParity(vector<int>& nums) {
+//         sort(nums.begin(), nums.end(), cmp);
+//         return nums;
+//     }
+// };
+
 class Solution {
 public:
-    static bool cmp(int a, int b) {
-        return (a % 2) < (b % 2);
-    }
-
     vector<int> sortArrayByParity(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), cmp);
+        int left = 0, right = nums.size() - 1;
+        while(left <= right) {
+            if (nums[right] % 2 == 0) swap(nums[left++], nums[right]);
+            else right--;
+        }
         return nums;
     }
 };
