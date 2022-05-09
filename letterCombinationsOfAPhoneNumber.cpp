c0abf714d7439e6c390836394ac76209e4fb967b
aayushkabra1 <ayushkabra1212@gmail.com>
2022-05-09 18:36:19 +0530

    leetcode.com/problems/letter-combinations-of-a-phone-number

--- letterCombinationsOfAPhoneNumber.cpp
+++ letterCombinationsOfAPhoneNumber.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <deque>
 #include <map>
 using namespace std;
 
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        map<int, vector<char>> mapping;
-        mapping['2'] = {'a', 'b', 'c'};
-        mapping['3'] = {'d', 'e', 'f'};
-        mapping['4'] = {'g', 'h', 'i'};
-        mapping['5'] = {'j', 'k', 'l'};
-        mapping['6'] = {'m', 'n', 'o'};
-        mapping['7'] = {'p', 'q', 'r', 's'};
-        mapping['8'] = {'t', 'u', 'v'};
-        mapping['9'] = {'w', 'x', 'y', 'z'};
+        int n = digits.size();
+        if (n == 0) return {};
+        map<char, vector<char>> mp;
+        mp['2'] = {'a', 'b', 'c'};
+        mp['3'] = {'d', 'e', 'f'};
+        mp['4'] = {'g', 'h', 'i'};
+        mp['5'] = {'j', 'k', 'l'};
+        mp['6'] = {'m', 'n', 'o'};
+        mp['7'] = {'p', 'q', 'r', 's'};
+        mp['8'] = {'t', 'u', 'v'};
+        mp['9'] = {'w', 'x', 'y', 'z'};
 
-        vector<string> ans;
-        for (char digit : digits) {
-            
+        deque<string> q;
+        for (char c : mp[digits[0]]) {
+            string s; s.push_back(c);
+            q.push_back(s);
         }
-        return ans;
+        for (int i = 1; i < n; i++) {
+            int size = q.size();
+            while(size--) {
+                string temp = q.front(); q.pop_front();
+                for (int j = 0; j < mp[digits[i]].size(); j++) {
+                    q.push_back(temp + mp[digits[i]][j]);
+                }
+            }
+        }
+        return vector<string>({q.begin(), q.end()});
     }
 };
 
